66142701a6773734896bdb3ff4f6c1f7cc335398
ap-rmit <s3941672@student.rmit.edu.au>
2026-02-08 23:25:09 +1100

    Support for .env file

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,28 @@ import (
 	"os"
 
 	"github.com/ap-rmit/scraper-don/internal/auth"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	_ = godotenv.Load()
+}
 
 func main() {
 	// Enable debug logging if DEBUG environment variable is set
 	debug := os.Getenv("DEBUG") == "true"
 	tokenHandler := auth.GetInstanceWithDebug(debug)
 
+	taikoUsername := os.Getenv("TAIKO_USERNAME")
+	taikoPassword := os.Getenv("TAIKO_PASSWORD")
+
+	if taikoUsername == "" || taikoPassword == "" {
+		fmt.Println("TAIKO_USERNAME or TAIKO_PASSWORD is not set")
+		return
+	}
+
 	fmt.Println("Authenticating...")
-	token, err := tokenHandler.Authenticate(os.Getenv("TAIKO_USERNAME"), os.Getenv("TAIKO_PASSWORD"))
+	token, err := tokenHandler.Authenticate(taikoUsername, taikoPassword)
 	if err != nil {
 		fmt.Printf("Authentication failed: %v\n", err)
 	} else {
--- makefile
+++ makefile
@@ -1,4 +1,7 @@
-run:
+start:
 	go run cmd/main.go
 debug:
 	DEBUG=true go run cmd/main.go
+mod:
+	go mod tidy
+	go mod vendor
